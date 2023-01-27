eb3536e4bcebbf7660084c5995ea7588858554f6
Von <xvtran@hotmail.com>
2023-01-27 08:53:59 -0800

    contrast and shapes

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+let contrastToggle = false;
+const scaleFactor = 1 / 30;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll('.shape');
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; i++) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
 
 function contact(event) {
     event.preventDefault();
@@ -21,3 +48,13 @@ function contact(event) {
     })
 }
 
+let isModalOpen = false;
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
+}
